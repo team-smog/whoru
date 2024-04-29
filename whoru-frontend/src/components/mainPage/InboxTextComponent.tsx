caa5fada45c8f03bc7334d8758932d1c08467909
류태수 <taesu960625@gmail.com>
2024-04-29 09:42:58 +0900

    [FE] feat: 수신 텍스트 컴포넌트 UI 구현 #S10P31D203-138

--- whoru-frontend/src/components/mainPage/InboxTextComponent.tsx
+++ whoru-frontend/src/components/mainPage/InboxTextComponent.tsx
@@ -24,21 +24,6 @@ const InboxTextComponent = () => {
 
   return (
     <div className={styles.inboxTextComponent}>
-      {/* <div className="m-4 text-xs">
-        <div className="flex flex-1 justify-start items-center">
-          <h3 className="font-bold text-base">익명 이름</h3>
-          <p className="mx-3 text-gray-400">1분 전</p>
-        </div>
-        <div className="mx-5 mt-3 mb-4">
-          <p className="break-keep ">{content}</p>
-        </div>
-        <div className="flex flex-1 justify-around items-conter mb-3">
-          <button>답장하기</button>
-          <button>번역보기</button>
-          <button className="text-gray-400">신고</button>
-        </div>
-      </div>
-      <hr/> */}
       <div className={styles.inboxTextComponentHeader}>
         <div className={styles.inboxTextComponentHeaderText}>
           <p className={styles.inboxTextComponentHeaderTextTitle}>익명 메세지</p>
