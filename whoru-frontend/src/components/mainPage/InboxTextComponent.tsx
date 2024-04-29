3c66ad5bcdc3a5c7a60225dc97a2ff18aa78b07f
류태수 <taesu960625@gmail.com>
2024-04-29 09:42:37 +0900

    [FE] feat: 수신 텍스트 컴포넌트 UI 구현 #S10P31D203-138

--- whoru-frontend/src/components/mainPage/InboxTextComponent.tsx
+++ whoru-frontend/src/components/mainPage/InboxTextComponent.tsx
@@ -1,11 +1,16 @@
 import axios from 'axios'
 import React, { useEffect,useState } from 'react'
+import styles from './InboxTextComponent.module.css'
+import ulIcon from '../../assets/components/InboxTextComponent/text-component-ul-button.svg'
+import sqIcon from '../../assets/components/InboxTextComponent/text-component-sq-button.svg'
+import xIcon from '../../assets/components/InboxTextComponent/text-component-x-button.svg'
 
 const InboxTextComponent = () => {
   const [content, setContent] = useState<string>("")
 
   // 일단 랜덤 글 불러오기
   // TODO: 백엔드와 연동하여 실제 데이터를 받아오도록 수정
+  // React Query 사용
   useEffect(() => {
     axios.get('https://api.chucknorris.io/jokes/random')
     .then((res) => {
@@ -18,8 +23,8 @@ const InboxTextComponent = () => {
   }, [])
 
   return (
-    <div className="">
-      <div className="m-4 text-xs">
+    <div className={styles.inboxTextComponent}>
+      {/* <div className="m-4 text-xs">
         <div className="flex flex-1 justify-start items-center">
           <h3 className="font-bold text-base">익명 이름</h3>
           <p className="mx-3 text-gray-400">1분 전</p>
@@ -33,7 +38,28 @@ const InboxTextComponent = () => {
           <button className="text-gray-400">신고</button>
         </div>
       </div>
-      <hr/>
+      <hr/> */}
+      <div className={styles.inboxTextComponentHeader}>
+        <div className={styles.inboxTextComponentHeaderText}>
+          <p className={styles.inboxTextComponentHeaderTextTitle}>익명 메세지</p>
+          <p className={styles.inboxTextComponentHeaderTime}>1분전</p>
+        </div>
+        <div className={styles.inboxTextComponentHeaderIcons}>
+          <img src={ulIcon} alt="ul-icon" />
+          <img src={sqIcon} alt="sq-icon" />
+          <img src={xIcon} alt="x-icon" />
+        </div>
+      </div>
+      <div className={styles.inboxTextComponentBody}>
+        <div className={styles.inboxTextComponentBodyMain}>
+          <p className={styles.inboxTextComponentBodyMainText}>{content}</p>
+          <div className={styles.inboxTextComponentFooter}>
+            <button className={styles.inboxTextComponentFooterButton}>답장</button>
+            <button className={styles.inboxTextComponentFooterButton}>번역</button>
+            <button className={styles.inboxTextComponentFooterReportButton}>신고</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
