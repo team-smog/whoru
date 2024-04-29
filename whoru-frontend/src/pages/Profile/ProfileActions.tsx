2d0ace68f18a392f2596cb71d2f02052dafaf3ef
오승현 <ohsh9529@gmail.com>
2024-04-29 13:46:18 +0900

    프로필 UI 수정

--- whoru-frontend/src/pages/Profile/ProfileActions.tsx
+++ whoru-frontend/src/pages/Profile/ProfileActions.tsx
@@ -16,16 +16,16 @@ const ProfileActions = () => {
 
 	return (
 		<div className="flex flex-row h-24 justify-between items-center px-4">
-			<div className="flex flex-col items-center justify-center w-28 h-20" onClick={handleAnnouncementClick}>
-				<img className="w-7 h-8 pb-1" src={Announcement} alt="Announcement" />
+			<div className="flex flex-col items-center justify-center w-28 h-20 gap-2"   onClick={handleAnnouncementClick}>
+				<img className="w-7 h-8" src={Announcement} alt="Announcement" />
 				<p className="text-xs">공지사항</p>
 			</div>
-			<div className="flex flex-col items-center justify-center w-28 h-20" onClick={handleCharacterCollectionClick}>
-				<img className="w-6 h-8 pb-2" src={Randombox} alt="Randombox" />
+			<div className="flex flex-col items-center justify-center pt-1 w-28 h-20 gap-3" onClick={handleCharacterCollectionClick}>
+				<img className="w-6 h-[26px]" src={Randombox} alt="Randombox" />
 				<p className="text-xs">캐릭터 도감</p>
 			</div>
-			<div className="flex flex-col items-center justify-center w-28 h-20">
-				<img className="w-7 h-8 pb-1" src={Logout} alt="Logout" />
+			<div className="flex flex-col items-center justify-center w-28 h-20 gap-2">
+				<img className="w-7 h-8" src={Logout} alt="Logout" />
 				<p className="text-xs">로그아웃</p>
 			</div>
 		</div>
