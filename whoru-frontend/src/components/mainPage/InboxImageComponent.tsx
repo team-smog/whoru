b39fdac3691c19bec1df9e5375ad1061be9362be
류태수 <taesu960625@gmail.com>
2024-04-29 11:09:52 +0900

    [FE]feat : 수신 이미지 컴포넌트 UI 구현 #S10P31D203-141

--- whoru-frontend/src/components/mainPage/InboxImageComponent.tsx
+++ whoru-frontend/src/components/mainPage/InboxImageComponent.tsx
@@ -1,31 +1,41 @@
 import React from 'react'
+import styles from './InboxImageComponent.module.css'
+import ulIcon from '../../assets/components/InboxImageComponent/image-component-ul-button.svg'
+import sqIcon from '../../assets/components/InboxImageComponent/image-component-sq-button.svg'
+import xIcon from '../../assets/components/InboxImageComponent/image-component-x-button.svg'
+
 
 const InboxImageComponent = () => {
   return (
-    <div className="">
-      <div className="m-4 text-xs">
-        <div className="flex flex-1 justify-start items-center">
-          <h3 className="font-bold text-base">익명 이름</h3>
-          <p className="mx-3 text-gray-400">1분 전</p>
+    <div className={styles.InboxImageComponent}>
+      <div className={styles.inboxImageComponentHeader}>
+        <div className={styles.inboxImageComponentHeaderText}>
+          <p className={styles.inboxImageComponentHeaderTextTitle}>익명 메세지</p>
+          <p className={styles.inboxImageComponentHeaderTime}>1분전</p>
         </div>
-        <div className="mx-5 mt-3 mb-4 flex justify-center">
-          {/* 
+        <div className={styles.inboxImageComponentHeaderIcons}>
+          <img src={ulIcon} alt="ul-icon" />
+          <img src={sqIcon} alt="sq-icon" />
+          <img src={xIcon} alt="x-icon" />
+        </div>
+      </div>
+      <div className={styles.inboxImageComponentBody}>
+        {/* 
             일단 랜덤 사진 불러오기
             TODO: 백엔드와 연동하여 실제 데이터를 받아오도록 수정
             React Query 사용
           */}
           <img 
-            className='min-w-[100px] max-w-[360px] min-h-[100px] max-h-[500px] rounded-lg shadow-[0_4px_8px_-4px_rgba(0,0,0,0.3)]'
+            // className='min-w-[100px] max-w-[360px] min-h-[100px] max-h-[500px]'
+            className={styles.inboxImageComponentBodyMain}
             src="https://source.unsplash.com/random"
             alt="이미지"
           />
-        </div>
-        <div className="flex flex-1 justify-around items-conter mb-3">
-          <button>답장하기</button>
-          <button className="text-gray-400">신고</button>
-        </div>
       </div>
-      <hr/>
+      <div className={styles.inboxImageComponentFooter}>
+        <button className={styles.inboxImageComponentFooterButton}>답장</button>
+        <button className={styles.inboxImageComponentFooterReportButton}>신고</button>
+      </div>
     </div>
   )
 }
