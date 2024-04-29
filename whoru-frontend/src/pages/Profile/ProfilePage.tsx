49de24834f9b44c1ac31e7ec0285646dca33fb02
오승현 <ohsh9529@gmail.com>
2024-04-29 09:43:47 +0900

    프로필 UI 수정

--- whoru-frontend/src/pages/Profile/ProfilePage.tsx
+++ whoru-frontend/src/pages/Profile/ProfilePage.tsx
@@ -30,11 +30,11 @@ const ProfilePage = () => {
 		navigate('/announcement')
 	}
 
-  const handleCharacterCollectionClick = () => {
+	const handleCharacterCollectionClick = () => {
 		navigate('/chacollection')
 	}
 
-  const handleInquiryClick = () => {
+	const handleInquiryClick = () => {
 		navigate('/Inquiry')
 	}
 
@@ -47,56 +47,61 @@ const ProfilePage = () => {
 			<div>
 				<Header info={info} />
 			</div>
-			<div className="ml-20">
+			<div className="pl-20">
 				<div className="flex flex-row">
-					<div className="mt-20 w-20 h-20">
+					<div className="pt-20 w-20 h-20">
 						<img src={Profile} alt="Profile" />
 					</div>
-					<div className="mt-20 ml-6">
-						<p className="text-xl mt-2">유저이름</p>
-						<p className="text-sm mt-1">유저이메일</p>
+					<div className="pt-20 pl-6">
+						<p className="text-xl pt-2">유저이름</p>
+						<p className="text-xs pt-1">유저이메일</p>
 					</div>
 				</div>
 			</div>
-			<hr className="border-1 border-black mt-10 mx-10" />
-			<div className="flex flex-row justify-between mt-4 mx-4">
+			<hr className="border-1 border-black mt-10 px-10" />
+			<div className="flex flex-row justify-between pt-4 px-4">
 				<div className="flex flex-col items-center justify-center w-28 h-20" onClick={handleAnnouncementClick}>
 					<img className="w-10 h-10" src={Announcement} alt="Announcement" />
 					<p className="text-xs">공지사항</p>
 				</div>
 				<div className="flex flex-col items-center justify-center w-28 h-20" onClick={handleCharacterCollectionClick}>
-					<img className="w-8 h-8 mb-2" src={Randombox} alt="Randombox" />
+					<img className="w-8 h-8 pb-2" src={Randombox} alt="Randombox" />
 					<p className="text-xs">캐릭터 도감</p>
 				</div>
 				<div className="flex flex-col items-center justify-center w-28 h-20">
 					<img className="w-10 h-10" src={Logout} alt="Logout" />
-					<p className="text-xs">로그 아웃</p>
+					<p className="text-xs">로그아웃</p>
 				</div>
 			</div>
-			<hr className="border-1 border-black mt-5 mx-10" />
-			<p className="ml-12 mt-4 text-xl">언어 설정</p>
-			<p className="ml-12 mt-4 text-xl" onClick={handleNotificationSettingsClick}>
+			<hr className="border-1 border-black pt-5 px-10" />
+			<p className="pl-12 pt-4">언어 설정</p>
+			<p className="pl-12 pt-4" onClick={handleNotificationSettingsClick}>
 				푸시 알림 설정
 			</p>
-			<p className="ml-12 mt-4 text-xl" onClick={handleInquiryClick}>문의하기</p>
+			<p className="pl-12 pt-4" onClick={handleInquiryClick}>
+				문의하기
+			</p>
 			{isModalOpen && (
 				<div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 ">
-					<div className="w-80 h-32 bg-white rounded-lg border-2 border-black">
-						<div className="flex flex-row justify-between bg-[#D78DDD]">
-							<h2 className="font-bold mt-1 p-2 ">푸시 알림 설정</h2>
-							<button className="mr-3 text-lg p-0" onClick={() => setIsModalOpen(false)}>
+					<div className="w-80 h-32 bg-white rounded-lg border-solid border-2 border-black">
+						<div className="flex flex-row justify-between rounded-t-lg bg-[#D78DDD]">
+							<h2 className="pt-1 p-2 ">푸시 알림 설정</h2>
+							<button className="pr-3 text-lg p-0" onClick={() => setIsModalOpen(false)}>
 								<img src={Cancel} alt="Cancel" />
 							</button>
 						</div>
-						<hr className="border-2 border-black" />
-						<p className="mt-3 ml-3 text-sm">현재 연결된 기기 : 기기명</p>
-						<p className="mt-3 ml-3 text-sm">
-							푸시 알림 
+						<hr className="border-1 border-black" />
+						<div className="flex flex-row">
+							<p className="pt-3 pl-3 text-sm">현재 연결된 기기</p>
+							<p className="pt-3 pl-3 text-sm text-[#CCCCCC]">기기명</p>
+						</div>
+						<p className="pt-3 pl-3 text-sm">
+							푸시 알림
 							<label className="toggle-container ml-5">
 								<input type="checkbox" onChange={handleToggleChange} checked={isPushNotificationEnabled} />
 								<span className="toggle-slider"></span>
 							</label>
-						</p>{' '}
+						</p>
 					</div>
 				</div>
 			)}
