c7c0bd7bedca2035b303843f268e890654ee28cf
강정수 <bbbbqz@nate.com>
2024-05-02 10:09:50 +0900

    [FE] feat:더미데이터 기입 #S10P31D203-220

--- whoru-frontend/src/components/Admin/InquiryPage.tsx
+++ whoru-frontend/src/components/Admin/InquiryPage.tsx
@@ -1,8 +1,27 @@
+type InquiryData = {
+  id:number;
+  content:string;
+  date:string;
+}
+
 const InquiryPage = () => {
+  const dummies:InquiryData[] = [
+    {
+      id: 1,
+      content:'[공지] 업데이트 사항',
+      date: '2024.04.24',
+    },
+  ]
+
   return (
-    <>
-      문의사항
-    </>
+    <div>
+      {dummies.map((item, id) => (
+        <div key={id}>
+          {item.content}
+          {item.date}
+        </div>
+      ))}
+    </div>
   )
 }
 
