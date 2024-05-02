b935e26c560abfc2175f7d30b6128d6f9b5d3483
Geishaz <allmin9702@naver.com>
2024-05-02 18:02:22 +0900

    [BE] fix : test code 주석처리#S10P31D203-179

--- whoru-backend/src/test/java/com/ssafy/whoru/TestPrepare.java
+++ whoru-backend/src/test/java/com/ssafy/whoru/TestPrepare.java
@@ -1,66 +1,66 @@
-package com.ssafy.whoru;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.ssafy.whoru.domain.collect.dao.IconRepository;
-import com.ssafy.whoru.domain.member.dao.MemberRepository;
-import com.ssafy.whoru.domain.message.dao.MessageRepository;
-import com.ssafy.whoru.domain.report.dao.ReportRepository;
-import com.ssafy.whoru.global.util.RedisUtil;
-import com.ssafy.whoru.util.MemberTestUtil;
-import com.ssafy.whoru.util.MessageTestUtil;
-import com.ssafy.whoru.util.ReportTestUtil;
-import org.junit.jupiter.api.BeforeEach;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
-import software.amazon.awssdk.services.s3.S3Client;
-import static org.mockito.Mockito.*;
-
-@ActiveProfiles("test")
-@SpringBootTest
-@AutoConfigureMockMvc
-public class TestPrepare {
-
-    @Autowired
-    protected MockMvc mockMvc;
-
-    @Autowired
-    protected ObjectMapper objectMapper;
-
-    @Autowired
-    protected RedisUtil redisUtil;
-
-    @Autowired
-    protected MessageRepository messageRepository;
-
-    @Autowired
-    protected MemberRepository memberRepository;
-
-    @Autowired
-    protected IconRepository collectRepository;
-
-    @Autowired
-    protected MemberTestUtil memberTestUtil;
-
-    @Autowired
-    protected MessageTestUtil messageTestUtil;
-
-    @Autowired
-    protected ReportTestUtil reportTestUtil;
-
-    @Autowired
-    protected ReportRepository reportRepository;
-
-    @MockBean
-    protected S3Client s3Client;
-
-    @BeforeEach
-    public void mockBeanSetup(){
-        reset(s3Client);
-    }
-
-
-}
+//package com.ssafy.whoru;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.ssafy.whoru.domain.collect.dao.IconRepository;
+//import com.ssafy.whoru.domain.member.dao.MemberRepository;
+//import com.ssafy.whoru.domain.message.dao.MessageRepository;
+//import com.ssafy.whoru.domain.report.dao.ReportRepository;
+//import com.ssafy.whoru.global.util.RedisUtil;
+//import com.ssafy.whoru.util.MemberTestUtil;
+//import com.ssafy.whoru.util.MessageTestUtil;
+//import com.ssafy.whoru.util.ReportTestUtil;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.test.context.ActiveProfiles;
+//import org.springframework.test.web.servlet.MockMvc;
+//import software.amazon.awssdk.services.s3.S3Client;
+//import static org.mockito.Mockito.*;
+//
+//@ActiveProfiles("test")
+//@SpringBootTest
+//@AutoConfigureMockMvc
+//public class TestPrepare {
+//
+//    @Autowired
+//    protected MockMvc mockMvc;
+//
+//    @Autowired
+//    protected ObjectMapper objectMapper;
+//
+//    @Autowired
+//    protected RedisUtil redisUtil;
+//
+//    @Autowired
+//    protected MessageRepository messageRepository;
+//
+//    @Autowired
+//    protected MemberRepository memberRepository;
+//
+//    @Autowired
+//    protected IconRepository collectRepository;
+//
+//    @Autowired
+//    protected MemberTestUtil memberTestUtil;
+//
+//    @Autowired
+//    protected MessageTestUtil messageTestUtil;
+//
+//    @Autowired
+//    protected ReportTestUtil reportTestUtil;
+//
+//    @Autowired
+//    protected ReportRepository reportRepository;
+//
+//    @MockBean
+//    protected S3Client s3Client;
+//
+//    @BeforeEach
+//    public void mockBeanSetup(){
+//        reset(s3Client);
+//    }
+//
+//
+//}
