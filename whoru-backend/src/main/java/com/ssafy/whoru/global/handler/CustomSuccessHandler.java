eb173f0d538dbef040ee086ba1da140e201dd4cc
Geishaz <allmin9702@naver.com>
2024-05-10 16:54:50 +0900

    [BE] hotfix: 로그인 redirect 주소 서버로 변경

--- whoru-backend/src/main/java/com/ssafy/whoru/global/handler/CustomSuccessHandler.java
+++ whoru-backend/src/main/java/com/ssafy/whoru/global/handler/CustomSuccessHandler.java
@@ -66,9 +66,8 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         //Response 세팅
         response.addCookie(createCookie("Refresh", refreshToken));
 
-        log.info("redirect url : "+testUrl);
-//        response.sendRedirect(url +"callback" + "?accessToken=" + accessToken);
-        response.sendRedirect(testUrl +"callback" + "?accessToken=" + accessToken);
+        response.sendRedirect(url +"callback" + "?accessToken=" + accessToken);
+//        response.sendRedirect(testUrl +"callback" + "?accessToken=" + accessToken);
     }
 
     private Cookie createCookie(String key, String value) {
