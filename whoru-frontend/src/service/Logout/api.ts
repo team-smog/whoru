b01fcca8c1269daae5b85cfc2fd9bed18d5d269b
강정수 <bbbbqz@nate.com>
2024-05-11 16:28:36 +0900

    [FE] feat: FCM 토큰 반환 #S10P31D203-252

--- whoru-frontend/src/service/Logout/api.ts
+++ whoru-frontend/src/service/Logout/api.ts
@@ -1,15 +1,14 @@
-import { axiosCookie } from "@/apis/axiosInstance";
+import { axiosAuthInstance } from '@/apis/axiosInstance'
 
 export const logoutApi = async () => {
-  const accesstoken = localStorage.getItem('AccessToken');
-  console.log(accesstoken)
-  const res = await axiosCookie.post(`/member/logout`, {
-    headers: {
-      'Authorization': `Bearer ${accesstoken}`,
-    },
-    withCredentials:true
-  });
-  console.log(res.data)
+	const accesstoken = localStorage.getItem('AccessToken')
+	console.log(accesstoken)
+	const res = await axiosAuthInstance.get(`/member/logout?fcmToken=${localStorage.getItem('FCMToken')}`, {
+		headers: {
+			Authorization: `Bearer ${accesstoken}`,
+		},
+	})
+	console.log(res.data);
 
-  return res.data;
+	return res.data;
 }
