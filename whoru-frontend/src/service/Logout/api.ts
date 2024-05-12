3a914670274f22cac7653114f27e60950d34791c
강정수 <bbbbqz@nate.com>
2024-05-13 00:33:33 +0900

    [FE] fix: 로그아웃 문제 수정

--- whoru-frontend/src/service/Logout/api.ts
+++ whoru-frontend/src/service/Logout/api.ts
@@ -3,7 +3,10 @@ import { axiosAuthInstance } from '@/apis/axiosInstance'
 export const logoutApi = async () => {
 	const accesstoken = localStorage.getItem('AccessToken')
 	console.log(accesstoken)
-	const res = await axiosAuthInstance.get(`/member/logout?fcmToken=${localStorage.getItem('FCMToken')}`, {
+	const res = await axiosAuthInstance.get(`/member/logout`, {
+		params: {
+			fcmToken: `fcmToken=${localStorage.getItem('FCMToken')}`
+		},
 		headers: {
 			Authorization: `Bearer ${accesstoken}`,
 		},
