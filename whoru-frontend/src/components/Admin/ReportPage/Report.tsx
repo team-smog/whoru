0b94615b69f7a8b5c18c5c1dbd793bc070c24f6b
강정수 <bbbbqz@nate.com>
2024-05-13 00:16:30 +0900

    [FE] feat: 신고사항 조회 api 연결 #S10P31D203-281

--- whoru-frontend/src/components/Admin/ReportPage/Report.tsx
+++ whoru-frontend/src/components/Admin/ReportPage/Report.tsx
@@ -1,52 +1,39 @@
-import { ReportInfo } from '@/pages/Admin/AdminPage';
-import { useState } from 'react';
-import ReportModal from './ReportModal';
+import ReportItems from './ReportItems'
+import { useIntersectionObserver } from '@/hooks/useIntersectionObserver'
+import { useReportDetail } from '@/hooks/Admin/useAdmin'
 
-const categories = ['전체', '비속어', '음란', '스팸', '광고'];
+// const categories = ['전체', '비속어', '음란', '스팸', '광고'];
 
-const Report = ({ data }: { data: ReportInfo[] }) => {
-  const [selectedCategory, setSelectedCategory] = useState<string>('전체');
-  const [isReportModalOpen, setIsReportModalOpen] = useState<boolean>(false);
-  const [selectedReport, setSelectedReport] = useState<ReportInfo | null>(null);
+const Report = () => {
+  const { data: reportData, fetchNextPage, hasNextPage } = useReportDetail();
+  const { setTarget } = useIntersectionObserver({ fetchNextPage, hasNextPage });
+  
+	// const [selectedCategory, setSelectedCategory] = useState<string>('전체')
+	// const [selectedReport, setSelectedReport] = useState<ReportInfo | null>(null);
 
-  // 신고내역 필터링
-  const filteredData = data.filter(item => {
-    if (selectedCategory === '전체') return true;
-    return item.report_type === selectedCategory;
-  });
+	// // 신고내역 필터링
+	// const filteredData = data.filter(item => {
+	//   if (selectedCategory === '전체') return true;
+	//   return item.report_type === selectedCategory;
+	// });
 
 
-  const handleReportClick = (report: ReportInfo) => {
-      // 신고내역 클릭 시 데이터 넘기기 위해
-    setSelectedReport(report);
-    setIsReportModalOpen(true);
-  }
 
-  return (
-    <>
-      <div className="flex justify-around pb-2 text-text_color">
-        {categories.map(category => (
-          <button
-            key={category}
-            className={`${selectedCategory === category ? 'text-text_color border-b-[0.5px] border-text_color' : 'text-gray-400'}`}
-            onClick={() => setSelectedCategory(category)}
-          >
-            {category}
-          </button>
-        ))}
-      </div>
-      <div>
-        {filteredData.map((item) => (
-          <div key={item.id} className="p-2 mx-4 border-b-[0.5px] border-gray-200" onClick={() => handleReportClick(item)}>
-            <p className='text-text_color text-[14px]'>[{item.report_type}] 신고사항 {item.message_id}</p>
-            <p className='text-[12px] text-gray-400'>{item.report_date}</p>
-          </div>
-        ))}
-      </div>
-      {isReportModalOpen && selectedReport && <ReportModal report={selectedReport} onClose={() => setIsReportModalOpen(false)} />}
-    </>
-  );
-};
+	return (
+		<>
+			<div className="w-full max-w-[500px]">
+				<div className="h-[calc(100vh-180px)] overflow-y-auto">
+					{reportData &&
+						reportData.pages.map((page) =>
+							page.content.map((item, index) => {
+								return <ReportItems key={index} data={item}/>
+							})
+						)}
+        <div ref={setTarget} className='h-[2rem]'></div>
+				</div>
+			</div>
+		</>
+	)
+}
 
 export default Report;
-
--- whoru-frontend/src/components/Admin/ReportPage/ReportModal.tsx
+++ whoru-frontend/src/components/Admin/ReportPage/ReportModal.tsx
@@ -1,24 +1,23 @@
-import Modal from "@/components/@common/Modal";
-import { ReportInfo } from "@/pages/Admin/AdminPage";
+// import Modal from "@/components/@common/Modal";
 
-const ReportModal = ({ report, onClose }: {report: ReportInfo, onClose: () => void }) => {
-  return (
-    <Modal width="300px" height="auto" title="신고 내역" onClose={onClose}>
-      <div className="flex flex-col px-4 pt-4 pb-4 text-text_color">
-        <div className="flex items-center gap-1">
-          <div className="text-[14px]">[{report.member_id}]</div>
-          <div className="flex text-red-500 text-[12px] items-end"> 2회</div>
-        </div>
-        <div className="flex border-b items-center gap-1">
-          <div className="text-[12px]">신고 일시</div>
-          <div className="text-gray-400 py-1 text-[12px] ">{report.report_date}</div>
-        </div>
-        <div className="text-[14px] pt-2">신고 사유</div>
-        <div className="w-full max-w-[500px] px-2 py-2 mt-2 border-[0.5px] rounded-[10px]">{report.content}</div>
-        <button className="w-full max-w-[500px] mt-5 py-1 bg-gray-200 rounded-[10px] border">무고벤</button>
-      </div>
-    </Modal>
-  )
-};
+// const ReportModal = ({ onClose } : { onClose: () => void }) => {
+//   return (
+//     <Modal width="300px" height="auto" title="신고 내역" onClose={onClose}>
+//       <div className="flex flex-col px-4 pt-4 pb-4 text-text_color">
+//         <div className="flex items-center gap-1">
+//           <div className="text-[14px]">[{data.member_id}]</div>
+//           <div className="flex text-red-500 text-[12px] items-end"> 2회</div>
+//         </div>
+//         <div className="flex border-b items-center gap-1">
+//           <div className="text-[12px]">신고 일시</div>
+//           <div className="text-gray-400 py-1 text-[12px] ">{data.report_date}</div>
+//         </div>
+//         <div className="text-[14px] pt-2">신고 사유</div>
+//         <div className="w-full max-w-[500px] px-2 py-2 mt-2 border-[0.5px] rounded-[10px]">{data.content}</div>
+//         <button className="w-full max-w-[500px] mt-5 py-1 bg-gray-200 rounded-[10px] border">무고벤</button>
+//       </div>
+//     </Modal>
+//   )
+// };
 
-export default ReportModal;
+// export default ReportModal;
