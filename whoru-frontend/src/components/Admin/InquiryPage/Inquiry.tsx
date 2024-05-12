f09b58db0a370bfe53f62532501d9ea86d56119b
강정수 <bbbbqz@nate.com>
2024-05-12 22:07:29 +0900

    [FE] 문의사항리스트 무한스크롤 구현 #S10P31D203-280

--- whoru-frontend/src/components/Admin/InquiryPage/Inquiry.tsx
+++ whoru-frontend/src/components/Admin/InquiryPage/Inquiry.tsx
@@ -1,54 +1,59 @@
-import { InquiryInfo } from '@/pages/Admin/AdminPage';
-import InquiryItems from './InquiryItems';
-import { useState } from 'react';
-import InquiryModal from './InquiryModal';
+import InquiryItems from './InquiryItems'
+import { useState } from 'react'
+import InquiryModal from './InquiryModal'
+import { useInquiryDetail } from '@/hooks/Admin/useAdmin'
+import { useIntersectionObserver } from '@/hooks/useIntersectionObserver'
 
-const Inquiry = ({ data }: { data: InquiryInfo[] }) => {
-    const [boardId, setBoardId] = useState<number | null>(null);
-    const [isInquiryModalOpen, setIsInquiryModalOpen] = useState<boolean>(false);
-    const [inquiryId, setInquiryId] = useState<number | null>(null);
+const Inquiry = () => {
+	const [boardId, setBoardId] = useState<number | null>(null)
+	const [isInquiryModalOpen, setIsInquiryModalOpen] = useState<boolean>(false)
+	const [inquiryId, setInquiryId] = useState<number | null>(null)
+	const { data: inquiryData, fetchNextPage, hasNextPage } = useInquiryDetail()
+  const { setTarget } = useIntersectionObserver({ fetchNextPage, hasNextPage });
 
-    const handleOpenModal = () => {
-        if (inquiryId !== null) {
-            setBoardId(inquiryId);
-            setIsInquiryModalOpen(true);
-        }
-    };
+	const handleOpenModal = () => {
+		if (inquiryId !== null) {
+			setBoardId(inquiryId)
+			setIsInquiryModalOpen(true)
+		}
+	}
 
-    const handleInquiryClick = (id: number) => {
-			setInquiryId(id);
-    };
+  const handleSelectItem = (id:number) => {
+    setInquiryId(id);
+  }
 
-    return (
-        <>
-            <div className="w-full max-w-[500px]">
-                <div className='h-[calc(100vh-180px)] overflow-y-auto'>
-                    {data.map((item, index) => (
-                        <div key={index} className="mb-4 cursor-pointer" onClick={() => handleInquiryClick(item.id)}>
-                            <InquiryItems data={item} />
-                        </div>
-                    ))}
-                </div>
-                {inquiryId && (
-                    <button
-                        className="fixed bottom-5 left-1/2 transform -translate-x-1/2 flex items-center justify-center w-[300px] h-8 bg-gray-200 text-[14px] rounded-[10px] text-text_color"
-                        onClick={handleOpenModal}
-                    >
-                        답글 작성하기
-                    </button>
-                )}
-                {isInquiryModalOpen && (
-                    <InquiryModal
-                        boardId={boardId}
-                        onClose={() => {
-                            setIsInquiryModalOpen(false);
-                            setInquiryId(null);
-                        }}
-                    />
-                )}
-            </div>
-        </>
-    );
-};
+	return (
+		<>
+			<div className="w-full max-w-[500px]">
+				<div className="h-[calc(100vh-180px)] overflow-y-auto">
+					{inquiryData &&
+						inquiryData.pages.map((page) =>
+							page.content.map((item, index) => {
+								return <InquiryItems key={index} data={item} onSelectItem={handleSelectItem}/>
+							})
+						)}
+          <div ref={setTarget} className='h-[2rem]'></div>
+				</div>
+				{inquiryId && (
+					<button
+						className="fixed bottom-5 left-1/2 transform -translate-x-1/2 flex items-center justify-center w-[300px] h-8 bg-gray-200 text-[14px] rounded-[10px] text-text_color"
+						onClick={handleOpenModal}
+					>
+						답글 작성하기
+					</button>
+				)}
+				{isInquiryModalOpen && (
+					<InquiryModal
+						boardId={boardId}
+						onClose={() => {
+							setIsInquiryModalOpen(false)
+							setInquiryId(null)
+						}}
+					/>
+				)}
+			</div>
+		</>
+	)
+}
 
-export default Inquiry;
+export default Inquiry
