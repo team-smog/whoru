62f3ad2ed81613816081d4d6d0f9c306a66eb6d8
강정수 <bbbbqz@nate.com>
2024-05-13 23:18:53 +0900

    [FE] fix: 푸시알림 오류 수정 #S10P31D203-285

--- whoru-frontend/src/pages/Login/CallBackPage.tsx
+++ whoru-frontend/src/pages/Login/CallBackPage.tsx
@@ -1,49 +1,45 @@
-import { useAuthReq } from "@/hooks/Auth/useAuth";
-import { setBoxCount, setRole } from "@/stores/store";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useAuthReq } from '@/hooks/Auth/useAuth'
+import { setBoxCount, setPushAlarm, setRole } from '@/stores/store'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { useNavigate, useSearchParams } from 'react-router-dom'
 
 const CallBackPage = () => {
-  const navigate = useNavigate();
-  const [searchParams] = useSearchParams();
-  const dispatch = useDispatch();
-  const boxCount = useSelector((state:any) => state.user.boxCount);
-  const role = useSelector((state:any) => state.user.role);
-  const { data: userData, isError, isLoading } = useAuthReq();
+	const navigate = useNavigate()
+	const [searchParams] = useSearchParams()
+	const dispatch = useDispatch()
+	const { data: userData, isError, isLoading } = useAuthReq()
 
-  useEffect(() => {
-    const accessToken = searchParams.get('accessToken');
-    console.log(accessToken);
+	useEffect(() => {
+		const accessToken = searchParams.get('accessToken')
+		console.log(accessToken)
 
-    if (!accessToken) {
-      navigate('/login');
-      return;
-    }
+		if (!accessToken) {
+			navigate('/login')
+			return
+		}
 
-    localStorage.setItem('AccessToken', accessToken);
+		localStorage.setItem('AccessToken', accessToken)
 
-    if (isLoading) return; 
+		if (isLoading) return
 
-    if (isError) {
-      navigate('/login');
-      return;
-    }
+		if (isError) {
+			navigate('/login')
+			return
+		}
 
-    if (userData) {
-      try {
-        dispatch(setBoxCount(userData.boxCount));
-        dispatch(setRole(userData.role));
-        console.log(boxCount)
-        console.log(role)
-        navigate('/');
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  }, [navigate, searchParams, dispatch, userData, isError, isLoading]);
+		if (userData) {
+			console.log('lasiefjlsaejf', userData.pushAlarm)
+				dispatch(setBoxCount(userData.boxCount))
+				dispatch(setRole(userData.role))
+				dispatch(setPushAlarm(userData.pushAlarm))
+				navigate('/')
+			} else {
+				console.log('error')
+			}
+	}, [navigate, searchParams, dispatch, userData, isError, isLoading])
 
-  return <div>로그인 중...</div>;
-};
+	return <div>로그인 중...</div>
+}
 
-export default CallBackPage;
+export default CallBackPage
