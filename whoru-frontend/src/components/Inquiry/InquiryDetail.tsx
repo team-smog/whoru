a8d2496f84bae2eac7ff5db859b8901e97362e3f
오승현 <ohsh9529@gmail.com>
2024-05-14 10:06:56 +0900

    문의사항 & 푸시 알림 css수정

--- whoru-frontend/src/components/Inquiry/InquiryDetail.tsx
+++ whoru-frontend/src/components/Inquiry/InquiryDetail.tsx
@@ -81,69 +81,65 @@ function InquiryDetail() {
 				<Header info={info} />
 			</div>
 			<div className="flex flex-col justify-center items-center pt-10">
-				<div className="w-full max-w-4xl mx-auto">
-					{loading ? (
-						<p>Loading...</p>
-					) : inquiry ? (
-						<div>
-							<div className="border-b-[0.5px] mx-8">
-								<div className="w-full">
-									<div className="flex flex-col items-start">
-										<div className="flex flex-row">
-											<img className="w-8 h-8" src={Q} alt="" />
-											<div className="flex flex-col justify-evenly w-full px-4 pb-2">
-												<p className="text-[16px]">{inquiry.subject}</p>
-												<p className="text-[14px] text-[#858585]">{formatDate(inquiry.updateDate)}</p>
-											</div>
+				{loading ? (
+					<p>Loading...</p>
+				) : inquiry ? (
+					<div>
+						<div className="border-b-[0.5px] mx-8">
+							<div className="w-full">
+								<div className="flex flex-col items-start">
+									<div className="flex flex-row">
+										<img className="w-8 h-8" src={Q} alt="" />
+										<div className="flex flex-col justify-evenly w-full px-4 pb-2">
+											<p className="text-[16px]">{inquiry.subject}</p>
+											<p className="text-[14px] text-[#858585]">{formatDate(inquiry.updateDate)}</p>
 										</div>
 									</div>
 								</div>
-							</div>
-							<div className="flex flex-row px-12 pt-4">
-								<p className="text-xs word-wrap:break-word scrollable-content">{inquiry.content}</p>
-							</div>
-							{inquiry.comment && (
-								<div className="w-full">
-									<div className="mx-auto fixed bottom-52 overflow-y-auto">
-										<div className="border-b-[0.5px] mx-8">
-											<div className="flex justify-center items-center">
-												<div className="flex flex-row justify-start w-full pb-2">
-													<div>
-														<img className="w-8 h-8" src={A} alt="" />
-													</div>
-													<div className="flex flex-col justify-start px-4">
-														<p className="text-[16px]">{inquiry.comment.commenterName}</p>
-														<p className="text-[12px] text-[#858585]">{formatDate(inquiry.comment.createDate)}</p>
-													</div>
-												</div>
-											</div>
-										</div>
-										<div
-											className="w-full max-w-[500px] max-h-[300px] px-12 pt-2 min-h-20"
-											style={{
-												overflowWrap: 'break-word',
-												overflow: 'auto',
-												whiteSpace: 'normal',
-											}}
-										>
-											<p className="text-xs">{inquiry.comment.content}</p>
-										</div>
-									</div>
-								</div>
-							)}
-							<div className="fixed bottom-20 w-full max-w-[500px] m-auto px-4">
-								<button
-									className="p-2 text-sm w-full bg-[#C4AFF1] h-10 rounded-lg"
-									onClick={() => handleDelete(inquiry.id)}
-								>
-									삭제하기
-								</button>
 							</div>
 						</div>
-					) : (
-						<p>No data found.</p>
-					)}
-				</div>
+						<div className="flex flex-row px-12 pt-4">
+							<p className="text-xs word-wrap:break-word scrollable-content">{inquiry.content}</p>
+						</div>
+						{inquiry.comment && (
+							<div className="w-full max-w-[500px] mx-auto fixed bottom-52 overflow-y-auto">
+								<div className="border-b-[0.5px] mx-8">
+									<div className="flex justify-center items-center">
+										<div className="flex flex-row justify-start w-full pb-2">
+											<div>
+												<img className="w-8 h-8" src={A} alt="" />
+											</div>
+											<div className="flex flex-col justify-start px-4">
+												<p className="text-[16px]">{inquiry.comment.commenterName}</p>
+												<p className="text-[12px] text-[#858585]">{formatDate(inquiry.comment.createDate)}</p>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div
+									className="w-full max-w-[500px] max-h-[300px] px-12 pt-4 min-h-20"
+									style={{
+										overflowWrap: 'break-word',
+										overflow: 'auto',
+										whiteSpace: 'normal',
+									}}
+								>
+									<p className="text-xs">{inquiry.comment.content}</p>
+								</div>
+							</div>
+						)}
+						<div className="fixed bottom-20 w-full max-w-[500px] m-auto px-4">
+							<button
+								className="p-2 text-sm w-full bg-[#C4AFF1] h-10 rounded-lg"
+								onClick={() => handleDelete(inquiry.id)}
+							>
+								삭제하기
+							</button>
+						</div>
+					</div>
+				) : (
+					<p>No data found.</p>
+				)}
 			</div>
 			<NavigationBar />
 		</div>
