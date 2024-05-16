8d9de938db939d282a4339ef1aa8cca7f3129b41
Be-HinD <kr100475@gmail.com>
2024-05-16 14:58:49 +0900

    [BE] fix:security config 인가 엔드포인트 추가

--- whoru-backend/src/main/java/com/ssafy/whoru/global/config/security/SecurityConfig.java
+++ whoru-backend/src/main/java/com/ssafy/whoru/global/config/security/SecurityConfig.java
@@ -49,6 +49,7 @@ public class SecurityConfig{
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/").permitAll()
                         .requestMatchers("/login/**").permitAll()
+                        .requestMatchers("/member/regenerate-token").permitAll()
                         // Prometheus에서 오는 요청만 허용
                         .requestMatchers("/actuator/**").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
