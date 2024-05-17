7a6f5933b51ae15ee18c6873bc7e3a5a407433f0
강정수 <bbbbqz@nate.com>
2024-05-17 10:06:56 +0900

    [FE] style: 캐릭터 도감 스타일 변경

--- whoru-frontend/src/components/Chacollection/ChacollectionProfile.tsx
+++ whoru-frontend/src/components/Chacollection/ChacollectionProfile.tsx
@@ -102,18 +102,18 @@ const ChacollectionProfile: React.FC = () => {
 			</div>
 				<p className="text-xl pt-32 pl-4">캐릭터 도감</p>
 				<p className="text-xs pt-2 pl-4 text-[#797979]">원하는 캐릭터로 자신의 프로필을 바꿀 수 있어요.</p>
-				<div className=" overflow-y-auto mx-4 pb-20 h-[calc(100vh-420px)]">
+				<div className=" overflow-y-auto m-4 py-4 h-[calc(100vh-420px)]">
 					{Object.keys(groupedIcons).map((grade, gradeIndex) => (
 						<div key={gradeIndex} className="flex flex-col justify-center">
 							<h2 className="text-lg pt-4 pl-4">{`등급: ${gradeMapping[grade] || grade}`}</h2>
 							<div className="flex flex-wrap">
 								{groupedIcons[grade].map((icon, index) => (
 									<div
-										className={`profile-container ${!icon.isAvailable ? 'unavailable-icon' : ''}`}
+										className={`rounded-[10px] m-1 p-3 ${!icon.isAvailable ? 'unavailable-icon' : 'bg-[#f0f0f0]'}`}
 										key={index}
 										onClick={() => icon.isAvailable && changeIcon(icon.iconId, icon.iconUrl)}
 									>
-										<img src={icon.iconUrl} alt={`Profile Icon ${index}`}/>
+										<img src={icon.iconUrl} alt={`Profile Icon ${index}`} className='w-20 h-20'/>
 									</div>
 								))}
 							</div>
