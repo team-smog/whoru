17fe4fefbd3deeb04148115b504b66b8b455cb4d
강정수 <bbbbqz@nate.com>
2024-05-17 10:17:02 +0900

    [FE] style: 캐릭터 도감 스타일 수정

--- whoru-frontend/src/components/Chacollection/ChacollectionProfile.tsx
+++ whoru-frontend/src/components/Chacollection/ChacollectionProfile.tsx
@@ -102,7 +102,7 @@ const ChacollectionProfile: React.FC = () => {
 			</div>
 				<p className="text-xl pt-32 pl-4">캐릭터 도감</p>
 				<p className="text-xs pt-2 pl-4 text-[#797979]">원하는 캐릭터로 자신의 프로필을 바꿀 수 있어요.</p>
-				<div className=" overflow-y-auto m-4 py-4 h-[calc(100vh-420px)]">
+				<div className="w-full max-w-[500px] mx-auto overflow-y-auto px-4 pb-4 h-[calc(100dvh-420px)] flex flex-col items-center">
 					{Object.keys(groupedIcons).map((grade, gradeIndex) => (
 						<div key={gradeIndex} className="flex flex-col justify-center">
 							<h2 className="text-lg pt-4 pl-4">{`등급: ${gradeMapping[grade] || grade}`}</h2>
