2d1a522319e964ae050d0fe86e8a803e6b1fa159
강정수 <bbbbqz@nate.com>
2024-05-17 14:15:08 +0900

    [FE] style: 캐릭터도감 수정

--- whoru-frontend/src/components/Chacollection/ChacollectionProfile.tsx
+++ whoru-frontend/src/components/Chacollection/ChacollectionProfile.tsx
@@ -102,14 +102,14 @@ const ChacollectionProfile: React.FC = () => {
 			</div>
 				<p className="text-xl pt-32 pl-4">캐릭터 도감</p>
 				<p className="text-xs pt-2 pl-4 text-[#797979]">원하는 캐릭터로 자신의 프로필을 바꿀 수 있어요.</p>
-				<div className="w-full max-w-[500px] mx-auto overflow-y-auto px-4 pb-4 h-[calc(100dvh-420px)] flex flex-col items-center">
+				<div className="w-full max-w-[500px] overflow-y-auto px-4 pb-4 h-[calc(100dvh-420px)]">
 					{Object.keys(groupedIcons).map((grade, gradeIndex) => (
 						<div key={gradeIndex} className="flex flex-col justify-center">
 							<h2 className="text-lg pt-4 pl-4">{`등급: ${gradeMapping[grade] || grade}`}</h2>
-							<div className="flex flex-wrap">
+							<div className="grid grid-cols-3 gap-2">
 								{groupedIcons[grade].map((icon, index) => (
 									<div
-										className={`rounded-[10px] m-1 p-3 ${!icon.isAvailable ? 'unavailable-icon' : 'bg-[#f0f0f0]'}`}
+										className={`rounded-[10px] mx-auto flex justify-center items-center p-4 ${!icon.isAvailable ? 'unavailable-icon' : 'bg-[#f0f0f0]'}`}
 										key={index}
 										onClick={() => icon.isAvailable && changeIcon(icon.iconId, icon.iconUrl)}
 									>
